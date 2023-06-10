a325ad7dfd2a61051269918466031d7c50301bc6
Fahamin <fahamin00@gmail.com>
2023-06-10 16:48:34 +0600

    Initial commit

---
+++ app/src/main/java/com/women/workout/female/fougurndservice/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.women.workout.female.fougurndservice
+
+import android.app.ActivityManager
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val serviceIntent = Intent(this, MyForegroundService::class.java)
+        startForegroundService(serviceIntent)
+    }
+
+    fun foregroundServiceRunning(): Boolean {
+        val activityManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        for (service in activityManager.getRunningServices(Int.MAX_VALUE)) {
+            if (MyForegroundService::class.java.name == service.service.className) {
+                return true
+            }
+        }
+        return false
+    }
+}
